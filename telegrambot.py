746d4968d4f201d59d785728b4d2c65fdf73e6b5
silentfatez <41805687+silentfatez@users.noreply.github.com>
2019-04-11 14:06:39 +0800

    Edited message

--- telegrambot.py
+++ telegrambot.py
@@ -31,17 +31,25 @@ db = firebase.database()
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
+    alert='Got Command '+command
 
-    print ('Got command')
+    print (alert)
 
     if command == '/table1':
         table1time=db.child('table1time').get().val()
-        name1=db.child('name1').get().val()
-
-        bot.sendMessage(chat_id, str((table1time,name1)))
+        name1=db.child('table1time').get().val()
+        if string != 'Empty':
+            Message='table 1 is Occupied by '+name1
+        else:
+            Message='table 1 is Empty'
+        bot.sendMessage(chat_id, str(Message))
     elif command == '/table2':
         table2time=db.child('table2time').get().val()
-        name2=db.child('name2').get().val()
+        name2=db.child('table2time').get().val()
+        if string != 'Empty':
+            Message='table 2 is Occupied by '+name2
+        else:
+            Message='table 2 is Empty'
 
 
         bot.sendMessage(chat_id, str((table2time,name2)))
