0e00345efe6b3a77682b542ef45cbd557d43b7b2
Dionetta Young <dionettayoung@hotmail.com>
2019-04-24 18:25:23 +0800

    usir

--- usir.py
+++ usir.py
@@ -3,36 +3,57 @@ import time
 from libdw import pyrebase
 from secrets import firebasesecrets
 from libdw import sm
+
+
 tablenumber=1
 GPIO.setwarnings(False)
+
+#distancelimit is set to 100cm
 distancelimit=100
 GPIO.setmode(GPIO.BCM)
 
 
-url = firebasesecrets['url'] # URL to Firebase database
-apikey = firebasesecrets['apikey'] # unique token used for authentication
+url = firebasesecrets['url'] #URL to Firebase database
+apikey = firebasesecrets['apikey'] #unique token used for authentication
 
 config={
     "apiKey":apikey,
     "databaseURL":url,
 }
 class Sensors(sm.SM):
+
+    #sets initial state to be 'empty'
     start_state = 'Empty'
+
     def start(self):
         self.state = self.start_state
     def get_next_values(self, state, inp):
             dist = distance()
             iroutput=GPIO.input(IR)
             if state=='Empty':
+
+                #when distance between ultrasonicsensor and object decreases, it shows that there is an object or person on the path of the pulse
+                #when infrared sensor detects change in heat energy, it shows that there is a human presence/movement
+                #if either one of the conditions is met, table is assumed as occupied
+                #state of machine updated to 'occupied'
                 if dist<distancelimit or iroutput==True:
                     next_state='Occupied'
                     db.child("table"+str(tablenumber)).set(next_state)
+
+                #if no change in initial distance limit and infrared radiation level in table surroundings, table is assumed as empty
                 else:
                     state=state
             else:
+
+                #when state is 'occupied'
+                ##when distance between ultrasonicsensor and object increases beyond distancelimit, it shows that there is nothing along the path of pulse
+                #when infrared sensor doesn't detects change in heat energy, it shows that there is a lack of human presence/movement
+                #if both of the above conditions are met, table is assumed as empty
                 if dist>distancelimit and iroutput==False:
                     next_state='Empty'
                     db.child('table'+str(tablenumber)).set(next_state)
+
+                #if distance between ultrasonic sensor and object is still below distancelimit or the infrared sensor still detects infrared radiation change, table is assumed occupied
                 else:
                     next_state=state
 
@@ -43,33 +64,35 @@ db = firebase.database()
 TRIG = 13
 ECHO = 19
 IR=20
-GPIO.setup(TRIG,GPIO.OUT)# Trig pin of Ultrasonic setup
-GPIO.setup(ECHO,GPIO.IN)
-GPIO.setup(IR,GPIO.IN) #GPIO 14 -> IR sensor as input
+GPIO.setup(TRIG,GPIO.OUT) #Trig pin of Ultrasonic setup
+GPIO.setup(ECHO,GPIO.IN) #GPIO 19 -> Echo as input
+GPIO.setup(IR,GPIO.IN) #GPIO 13 -> IR sensor as input
 
 def distance():
-    # set Trigger to HIGH
+    #set Trigger to HIGH
     GPIO.output(TRIG, True)
 
-    # set Trigger after 0.01ms to LOW
+    #set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
 
+    #define the starting and stopping time
     StartTime = time.time()
     StopTime = time.time()
 
-    # save StartTime
+    #saving the starting time as the time when ultrasonic pulse is sent out
     while GPIO.input(ECHO) == 0:
         StartTime = time.time()
 
-    # save time of arrival
+    #saving the stopping time as the time when ultrasonic pulse is received back
     while GPIO.input(ECHO) == 1:
         StopTime = time.time()
 
-    # time difference between start and arrival
+    #time taken for pulse to be sent and reflected back and received
     TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
+
+    #multiply with the sonic speed (34300 cm/s) to get the distance travelled by the pulse
+    #since distance travelled is twice of distance between sensor and object, divide by 2.
     distance = (TimeElapsed * 34300) / 2
 
     return distance
@@ -84,8 +107,9 @@ try:
         while True:
             dist = distance()
             next_state=sensor.step(next_state)
+            #reading taken at 1 second intervals
             time.sleep(1)
-        # Reset by pressing CTRL + C
+#reset by pressing CTRL + C
 except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
