dc71928137bbd5479b9e4529041f6d92a2064d33
Dionetta Young <dionettayoung@hotmail.com>
2019-04-24 17:37:04 +0800

    add

--- telegrambot.py
+++ telegrambot.py
@@ -4,11 +4,12 @@ import datetime
 import telepot
 from telepot.loop import MessageLoop
 from libdw import pyrebase
+#secrets is a python file containing all the credentials of Firebase and Telegram
 from secrets import *
 
 
-url = firebasesecrets['url'] # URL to Firebase database
-apikey = firebasesecrets['apikey'] # unique token used for authentication
+url = firebasesecrets['url'] #URL to Firebase database
+apikey = firebasesecrets['apikey'] #unique token used for authentication
 
 config={
     "apiKey":apikey,
@@ -24,38 +25,54 @@ def handle(msg):
     alert='Got Command '+command
 
     print (alert)
+    #initialize the application
     if command=='/start':
         Message='Hi,welcome to table checker. You can type /help for some help.'
+    #instructions on how to use the application
     elif command=='/help':
          Message='Looks like you need some help. You can check the status of table 1 using /table1 and table 2 using /table2. You can also use /alltables to get an update on all tables available.'
-
+    #displays the availabilities for all tables from Firebase database
     elif command=='/alltables':
         name1=db.child('name1').get().val()
         name2=db.child('name2').get().val()
         if name1 != '':
+            #if name for table1 is not empty, table is occupied
             submessage1='Table 1 is Occupied by '+name1
         else:
+            #if name for table1 is empty, table is empty
             submessage1='Table 1 is Empty'
         if name2 !="":
+            #if name for table2 is not empty, table is occupied
             submessage2='Table 2 is Occupied by '+name2
         else:
+            #if name for table2 is empty, table is empty
             submessage2='Table 2 is Empty'
+
         Message=submessage1+' and '+submessage2
+    #if application user wants to check availability of table1
     elif command == '/table1':
+        #retrieves information from Firebase under key 'name1'
         name1=db.child('name1').get().val()
         if name1 != '':
+            #if name for table1 is not empty, table is occupied
             Message='Table 1 is Occupied by '+name1
         else:
+            #if name for table1 is empty, table is empty
             Message='Table 1 is Empty'
+    #if application user wants to check availability of table2
     elif command == '/table2':
+        #retrieves information from Firebase under key 'name2'
         name2=db.child('name2').get().val()
         if name2 != '':
+            #if name for table2 is not empty, table is occupied
             Message='Table 2 is Occupied by '+name2
         else:
+            #if name for table2 is empty, table is empty
             Message='Table 2 is Empty'
     else:
+        #if application user sends other commands, it will return an error message
         Message="wrong command, use /help for help."
-
+    #display message in SUTDROOMbot
     bot.sendMessage(chat_id, str(Message))
 
 bot = telepot.Bot(TOKEN)
